bbd74bf399599a2974c66234951f232a9653c09b
Etherlyvan <jovanku1@gmail.com>
2025-02-18 21:30:21 +0700

    Fix lang jp

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
 'use client';
 
 import Hero from '@/app/components/hero';
-import Footer from './components/Footer';
 
 export default function Home() {
     return (
         <div className='min-h-screen'>
             <main className='flex flex-col items-center sm:items-start gap-6'>
                 <Hero />
-                <Footer />
             </main>
         </div>
     );
